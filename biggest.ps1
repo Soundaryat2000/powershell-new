79a35dab8c018b8884a7bd546648570a03054fb6
Soundaryat2000 <94119115+Soundaryat2000@users.noreply.github.com>
2021-11-30 11:23:06 +0530

    Add files via upload

---
+++ biggest.ps1
@@ -0,0 +1,22 @@
+﻿$num1=read-host("Enter Num1")
+$num1
+$num2=read-host("Enter Num2")
+$num2
+$num3=read-host("Enter Num3")
+$num3
+
+if($num1 -gt $num2 )
+{
+ if($num1 -gt $num3 ){
+
+    write-host($num1)
+    }
+    }
+elseif($num2 -gt $num1){if($num2 -gt $num3)
+{
+
+    write-host($num2)
+    }
+    }
+else
+     {$num3}
